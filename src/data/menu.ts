fd946226b5376171d0b6672473420847da015b53
Kevin Torres <kevtorres.pro@gmail.com>
2025-10-30 09:12:36 -0600

    Add menuLinks data structure for navigation

---
+++ src/data/menu.ts
@@ -0,0 +1,20 @@
+const menuLinks = [
+  {
+    link: "#inicio",
+    name: "Inicio",
+  },
+  {
+    link: "#proyectos",
+    name: "Proyectos",
+  },
+  {
+    link: "#proceso",
+    name: "Proceso",
+  },
+  {
+    link: "#contacto",
+    name: "Contacto",
+  },
+];
+
+export default menuLinks;
